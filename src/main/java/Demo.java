4d97a3b474b16cf56536ab951cd81225c96026a3
85438 <85438@gmail.com>
2021-10-03 14:40:12 +0700

    add demo

--- src/main/java/Demo.java
+++ src/main/java/Demo.java
@@ -3,7 +3,6 @@ public class Demo {
 	public static void main(String[] args) {
 		for (int i = 1; i <= 7; i++) {
 			System.out.println(String.format("%d => Vi Bằng Xin Chào !", i));
-			
 		}
 	}
 }
